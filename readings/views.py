8d0b9f7d325a21c15b6643d04c329027d84b6337
Caleb Hayashida <santeyio@gmail.com>
2015-08-26 06:02:14 -0400

    readings views work

--- readings/views.py
+++ readings/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from .models import Schedule, Day
+from models import Schedule, Day
 
 
 def index(request):
 	readings = Schedule.objects.all()
-	template = loader.get_template('readings/index.html')
 	context = RequestContext(request, {
 		'readings': readings,
 	})
-	return HttpResponse(template.render(context))
+	return render(request, 'readings/index.html', context)
 
+def detail(request, reading_id):
+	reading = get_object_or_404(Schedule, pk=reading_id)
+	days = Day.objects.filter(schedule=reading_id)
+	context = {'reading': reading, 'days': days}
+	return render(request, 'readings/detail.html', context)
