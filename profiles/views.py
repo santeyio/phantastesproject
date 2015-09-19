c78a75f63de9cc00798a47e01266ff8c2da2e229
Caleb Hayashida <santeyio@gmail.com>
2015-09-19 16:31:07 -0400

    more stuff

--- profiles/views.py
+++ profiles/views.py
@@ -4,23 +4,24 @@ from django.template import RequestContext, loader
 from models import Profile
 from readings.models import Book, Day
 from django.contrib.auth.decorators import login_required
-import datetime
+import datetime, operator
 
 @login_required(login_url='/account/login')
 def index(request, username):
-	books = Book.objects.filter(active=True)
-	for book in books:
-		timedelta = datetime.date.today() - book.start_date
-		if timedelta.days < book.number_of_days:
-			current_book = book
-			days = Day.objects.filter(book=current_book)
-			break
-		else:
-			continue
+    books = Book.objects.filter(active=True)
+    for book in books:
+        timedelta =  datetime.date.today() - book.start_date 
+        if timedelta.days < book.number_of_days:
+            current_book = book
+            days = Day.objects.filter(book=current_book)
+            days = sorted(days, key=operator.attrgetter('day'), reverse=False)
+            break
+        else:
+            continue
 
-	context = RequestContext(request, {
-		'book': current_book,
-		'day': days[timedelta.days],
-	})
+    context = RequestContext(request, {
+        'book': current_book,
+        'day': days[timedelta.days],
+    })
 
-	return render(request, 'profiles/index.html', context)
+    return render(request, 'profiles/index.html', context)
