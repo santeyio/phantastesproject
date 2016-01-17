ce47495f36c6920d7e15ce46a098e4fc0e5df767
Caleb Hayashida <santeyio@gmail.com>
2016-01-16 20:05:02 -0500

    fix problem with profiles urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 	url(r'^add_comment/$', views.add_comment, name='add_comment'),
 	url(r'^get_user_feed/$', views.get_user_feed, name='get_user_feed'),
 	url(r'^get_comments/$', views.get_comments, name='get_comments'),
-	url(r'^post/(?P<post_id>[0-9]+)$', views.post_detail, name='post_detail'),
+	url(r'^post/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^(?P<username>.+)/$', views.index, name='index'),
 ]
