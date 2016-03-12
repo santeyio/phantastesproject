0a6232b2fdd0fda5c3ff904748ae353dd0d3a5cb
Caleb Hayashida <santeyio@gmail.com>
2016-03-12 07:59:20 -0500

    Added cronjob for daily email update checks

--- phantastes/settings.py
+++ phantastes/settings.py
@@ -227,7 +227,7 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = "Anodos <root@calebhayashida.com>"
+DEFAULT_FROM_EMAIL = "Phantastes <anodos@calebhayashida.com>"
 
 ACCOUNT_OPEN_SIGNUP = True
 ACCOUNT_EMAIL_UNIQUE = True
---
+++ polls/management/commands/cron.py
@@ -0,0 +1,46 @@
+# ----
+# daily cron for checking if any email notifications need to be sent out
+# -----
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from polls.models import Poll
+from datetime import date, timedelta
+
+
+def notify_all_users(poll, notification_type):
+
+    # get emails of all users
+    users = User.objects.all()
+    email_list = []
+    for user in users:
+        email_list.append(user.email)
+
+    subject = "Phantastes: " + notification_type
+    email_from = "Anodos <root@phantastes.calebhayashida.com>"
+    email_body = "this is the email body. It rocks, right?"
+    print subject
+    print email_list
+    
+    # msg = EmailMessage(subject, email_body, email_from, email_list)
+    # msg.content_subtype = "html"
+    # msg.send()
+                           
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        polls = Poll.objects.filter()
+
+        for poll in polls:
+            if poll.nominations_open.date() == date.today():
+                notify_all_users(poll, "nominations_open")
+            if poll.voting_open.date() == date.today():
+                notify_all_users(poll, "voting_open")
+            if poll.voting_close.date() - date.today() == timedelta(1):
+                notify_all_users(poll, "voting_close")
+
+
+
